94e38f78f2f2044b9218092fb3de9d8f38eb9273
Maxime Levesque <maxime.levesque@gmail.com>
2012-05-12 17:26:43 -0400

    demo

---
+++ app/assets/javascripts/votePanelDemo.js
@@ -0,0 +1,39 @@
+
+function createBallotView(ballotModel, rootElement, templates) {
+     var V = Backbone.View.extend({
+    	el: rootElement,
+    	model: ballotModel,
+        events: {
+            "click a[altId]" : "_scoreAlternative"
+        },
+        initialize: function() {
+        	
+            this.model.on('change', this.render, this);
+        },
+        _scoreAlternative: function(e) {
+
+        	var target = $(e.currentTarget);
+        	var altId = target.attr('altId');
+        	var score = target.attr('score');
+        },
+        render: function() {
+        	var ballot = _.extend({}, this.model.toJSON());
+        	var el = $(this.el);
+
+            el.html(templates.voteTemplate(ballot));
+
+            _.each(ballot.scores, function(score) {
+            	
+            	if(score.currentScore === 0 || score.currentScore) {
+            	  var e = el.find("a[score=" + score.currentScore + "][altId=" + score.alternativeId + "]")
+            	  e.button('toggle');
+            	}
+            })
+            
+
+            return this;
+        }
+    });
+
+    return new V()
+ }
