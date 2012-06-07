151dccafd127423f7e8e6f0634e2f582e2a4fd68
Maxime Levesque <maxime.levesque@gmail.com>
2012-06-07 08:40:48 -0400

    ...

--- app/com/decision_hub/Mailer.scala
+++ app/com/decision_hub/Mailer.scala
@@ -8,15 +8,12 @@ object Mailer {
 
   def sendConfirmationMail(content: String, recipientAddress: String) = {
 
-    //noreply@clearvote.net
-
-    
     val props = System.getProperties()
     props.put("mail.smtp.host", "smtp.webfaction.com")
     props.put("mail.debug", "true")
     props.put("mail.smtp.auth", "true");
-    props.put("mail.user", "all_clearvote@clearvote.net")
-    props.put("mail.password", "zaza123")
+    //props.put("mail.user", "all_clearvote@clearvote.net")
+    //props.put("mail.password", "zaza123")
 
     val s = Session.getInstance(props);
     val m = new MimeMessage(s)
@@ -32,16 +29,11 @@ object Mailer {
     val transport = s.getTransport("smtp")
     try {
       transport.connect("all_clearvote", "zaza123")
-    
       transport.sendMessage(m, m.getRecipients(Message.RecipientType.TO))
     }
     finally {
       transport.close();
     }
-
-  //s.connect('smtp.webfaction.com')
-  //s.login('all_clearvote','zaza123')
-
   }
 
   def main(args: Array[String]): Unit = {
