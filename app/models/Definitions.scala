d850d4ec0985fcb992e62f68922dae41a6f7598f
Maxime Levesque <maxime.levesque@gmail.com>
2012-06-23 11:36:01 -0400

    rm

--- app/models/Definitions.scala
+++
@@ -1,9 +0,0 @@
-package models
-
-
-object Defs {
-
-  val uninominalSystem = 
-    fr("Mode de scrutin uninominal","Mode de scrutin où l'électeur choisis une seule option, ce mode de scrutin est en vigueur au Canada.") ~
-    en("Uninominal voting system","Voting method where an elector can only chose one option")
-}
